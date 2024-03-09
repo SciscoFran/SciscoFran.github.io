70d64d70e4f542788c2a369a16fe31103e621b61
SciscoFran <133995417+SciscoFran@users.noreply.github.com>
2024-03-08 19:40:54 -0700

    Add files via upload

---
+++ Codigo.js
@@ -0,0 +1,11 @@
+var largoP = 500;
+
+function tamaño(nombre_clase){
+    largoP = largoP - 100;
+    imgR = document.getElementsByClassName(nombre_clase)[0];
+    if(largoP > 0){
+        imgR.style.height =  largoP+"px";
+        imgR.style.width = "auto";
+    }
+    
+}
